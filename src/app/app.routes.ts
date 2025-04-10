a06e27307ab8038b28e94855cad17592ce625b4e
lpmichael <lpm85920@colostate.edu>
2025-04-09 20:18:31 -0600

    fixed links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { FormComponent } from './form/form.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
-    {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
-    {path: 'form', component: FormComponent}
+    {path: 'form', component: FormComponent},
+    {path: '', redirectTo: 'home', pathMatch: "full"},
 ];
