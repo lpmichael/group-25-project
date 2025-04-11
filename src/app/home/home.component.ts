48ccd027b7cb2b325c967afb3d94faf4ee560c5a
lpmichael <lpm85920@colostate.edu>
2025-04-11 15:18:12 -0600

    moved form to home page, altered navigation to reflect, did some css changes, and removed a comma

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { FormComponent } from '../form/form.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './home.component.html',
   styleUrl: '../app.component.css'
 })
