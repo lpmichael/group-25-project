366e28aa052b10aa96c32d1feb24d7ae30e0580a
lpmichael <lpm85920@colostate.edu>
2025-04-11 16:54:39 -0600

    added event emitter to share form data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
@@ -8,6 +8,8 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+@Output() formEmit = new EventEmitter<FormGroup>();
+
   numsForm = new FormGroup({
     rows: new FormControl(0, [Validators.required, Validators.min(1)]),
     cols: new FormControl(0, [Validators.required, Validators.min(1)]),
@@ -17,6 +19,7 @@ export class HomeComponent {
      
       onSubmit() {
         console.log(this.numsForm.value);
+        this.formEmit.emit(this.numsForm); 
       }
 }
 
