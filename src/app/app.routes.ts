610ce5b2b3b1711a009efa4129b3a5572a2f523f
lpmichael <lpm85920@colostate.edu>
2025-04-14 12:29:10 -0600

    fixed the weird display and links, and images now work! all are in the public directory and are accessed by name alone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { UppertableComponent } from './uppertable/uppertable.component';
 import { ColorpaintingtableComponent } from './colorpaintingtable/colorpaintingtable.component';
+import { Component } from '@angular/core';
+
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: '', component: Component},
     {path: 'home', component: HomeComponent},
     {path: 'about', component: AboutComponent},
     {path: 'uppertable', component: UppertableComponent}, //Hopefully temporary: OO
