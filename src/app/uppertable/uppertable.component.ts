98079761a6c49c90d5f5cfbf02f294fec693fb50
Ocean O'Neil <oceans77@colostate.edu>
2025-05-06 16:18:42 -0600

    Options now populate from database (not synced yet)

--- src/app/uppertable/uppertable.component.ts
+++ src/app/uppertable/uppertable.component.ts
@@ -1,17 +1,20 @@
-import { Component, Input, EventEmitter, OnInit } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { Component, Input, EventEmitter, OnInit, NgModule } from '@angular/core';
+import { NgIf, NgFor } from '@angular/common';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HomeComponent } from '../home/home.component';
-import { NgFor } from '@angular/common';
+import { first, Observable } from 'rxjs';
 import { DatabaseService } from '../services/database.service';
-import { response } from 'express';
+import { Router } from '@angular/router';
+import { isNgTemplate } from '@angular/compiler';
+import { Colors } from '../colorsmodel/colors.model'; 
+
 
 
 @Component({
   selector: 'app-uppertable',
   templateUrl: './uppertable.component.html',
   styleUrl: './uppertable.component.css',
-  imports: [ReactiveFormsModule, HomeComponent, NgFor, NgIf],
+  imports: [ReactiveFormsModule, HomeComponent, NgFor, NgIf, FormsModule],
 })
 
 
@@ -20,11 +23,9 @@ import { response } from 'express';
 export class UppertableComponent implements OnInit{
     amount = 0; //how many rows of colors
     formSubmitted = false;
-    i= 0;
-    data: any;
+    colors:Array<Colors> = [];
+    selectedValue:Array<Colors> = [];
 
-    
-  
 
     colorForm!: FormGroup;
    
@@ -32,19 +33,25 @@ export class UppertableComponent implements OnInit{
    
     ngOnInit() {
       this.colorForm = this.fb.group({
-        allColors: new FormControl()
+        selectedColor: new FormControl()
       });
 
-      this.db.getAllColors(this.amount).subscribe({
+      this.getColorsFromDB(1, this.amount);
+      }
+    
+//
+    getColorsFromDB(firstTime: number, colorAmount: number){
+      this.db.getAllColors(colorAmount, firstTime).subscribe({
         next: (response) => {
-          this.data = response;
-          console.log ('Fetched: ', this.data);
+          this.colors = response;
+          console.log ('Fetched: ', this.colors);
         },
         error: (err) => {
           console.error('AHHHHHHHHH: ', err);
-        }
-      })
-    }
+        }})
+        
+      }
+
 
     counter (num: number){
       return new Array(num);
@@ -53,10 +60,19 @@ export class UppertableComponent implements OnInit{
     setAmountAndShow(ogData: FormGroup){
       this.amount = ogData.get('colors')?.value;
       this.formSubmitted = true;
-
+      this.getColorsFromDB(1, this.amount);
+      this.selectedValue = this.colors;
     }
 
+    disableOption(id: number){
+      this.selectedValue.forEach(element => {
+        
+      });
+    }
 
+    selected(i: number){
+      return this.colors[i].ID;
+    }
     }
    
   
