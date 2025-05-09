c16ddea388a3c88c443640c373be0bc8053cb8e2
Ocean O'Neil <oceans77@colostate.edu>
2025-05-09 17:26:47 -0600

    added update, add and delete calls

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Colors } from '../colorsmodel/colors.model';
-import { FormGroup } from '@angular/forms';
+import { Form, FormGroup } from '@angular/forms';
 import { response } from 'express';
 
 //TODO
@@ -18,22 +18,22 @@ import { response } from 'express';
 })
 export class DatabaseService {
 
-  private apiurl = 'https://cs.colostate.edu/~EID/databaseapi.php' //change if you need to test locally
+  private apiurl = 'https://cs.colostate.edu/~oceans77/databaseapi.php' //change if you need to test locally
   
 
   constructor(private http: HttpClient) {}
 
   //get specified colors... somehow
-  getAllColors(count: number, firstTime: number): Observable<Colors[]>{
+  getAllColors(): Observable<Colors[]>{
     let params = new HttpParams();
-    params = params.append("count", count);
-    params = params.append("first", firstTime);
+ 
+
     //params.append('first', firstTime);
-    return this.http.get<Colors[]>(this.apiurl, {params: params});
+    return this.http.get<Colors[]>(this.apiurl);
   }
 
 
-  updateColor(colorUpdate: FormGroup) {
+  updateColor(colorUpdate: FormGroup, colorsList: Array<Colors>) {
     let id = colorUpdate.get('color')?.value;
     let hex = colorUpdate.get('hex')?.value;
     let name = colorUpdate.get('name')?.value;
@@ -47,10 +47,53 @@ export class DatabaseService {
       name: name,
     }
 
-    return this.http.post<any>(this.apiurl, colorData, {params: params}).subscribe();
+    this.http.post<any>(this.apiurl, colorData, {params: params}).subscribe();
 
+    this.getAllColors().subscribe({
+      next: (response) => {
+        colorsList = response;
+            console.log ('Fetched: ', colorsList);
+          },
+          error: (err) => {
+            console.error('AHHHHHHHHH: ', err);
+          }})
+          return;
+
+        
 
   }
+
+  addColor(colorUpdate: FormGroup, colorsList: Array<Colors>){
+    console.log('hit service');
+
+    let id = colorUpdate.get('color')?.value;
+    let hex = colorUpdate.get('hex')?.value;
+    let name = colorUpdate.get('name')?.value;
+    
+    let params = new HttpParams();
+    params = params.append("add", 1);
+
+    const colorData = {
+      id: id,
+      hexval: hex,
+      name: name,
+    }
+
+    this.http.post<any>(this.apiurl, colorData, {params: params}).subscribe();
+
+    this.getAllColors().subscribe({
+      next: (response) => {
+        colorsList = response;
+            console.log ('Fetched: ', colorsList);
+          },
+          error: (err) => {
+            console.error('AHHHHHHHHH: ', err);
+          }})
+          return;
+
+        }
+   
+  
 
   getAmount(){
     let count = [];
@@ -65,6 +108,26 @@ export class DatabaseService {
     params = params.append('update', 1);
     return this.http.get<Colors>(this.apiurl, {params: params}).subscribe();
 
+  }
+
+  deleteColor(colorDelete: FormGroup){
+    console.log('hit delete');
+    let params = new HttpParams;
+    params = params.append('delete', 1);
+
+    let id = colorDelete.get('color')?.value;
+    let hex = colorDelete.get('hex')?.value;
+    let name = colorDelete.get('name')?.value;
+    
+
+    const colorData = {
+      id: id,
+      hexval: hex,
+      name: name,
+    }
+
+    this.http.post<any>(this.apiurl, colorData, {params: params}).subscribe();
+    return;
   }
 
   //
